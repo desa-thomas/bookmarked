8f622a6f868f4fb51c2d9a9519c9da2829334f91
tom <desa2thomas@gmail.com>
2026-03-18 17:18:27 -0400

    added baseline NavBar

--- bookmarked-frontend/src/App.jsx
+++ bookmarked-frontend/src/App.jsx
@@ -2,20 +2,26 @@ import { useState } from 'react'
 import './index.css'
 
 // Import router for pages
-import { HashRouter as Router, Routes, Route} from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 
+// Import Pages 
+
+// Import Components
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return(
-    <>
-     <div  
-    className='mx-auto flwx max-w-sm item-center gap-x-4 rounded-xl bg-blue p-6 shadow-lg outline outline-black/5'>
-      Hello world 
-     </div>
+  return (
+    <div className='font-sans'>
+      <NavBar />
 
-    </>)
+      <div
+        className='flex w-screen h-screen items-center justify-center'>
+        Placeholder
+      </div>
+
+    </div>)
 
 }
 
---
+++ bookmarked-frontend/src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+
+export default function NavBar() {
+
+  return (
+    <div
+      className="flex flex-row w-screen items-center justify-between border-2 border-black border-md p-4 mb-1 ">
+
+      <div className="justify-self-start text-2xl hover:underline">
+        Bookmarked
+      </div>
+
+      <div className="justify-self-end flex flex-row gap-5 items-center">
+        <p className="hover:underline">Books</p>
+        <p className="hover:underline">Lists</p>
+        <p className="hover:underline">Forums</p> 
+        <LoginButton/>
+      </div>
+    </div>
+  )
+}
+
+
+function LoginButton(){
+
+  return (
+    <button
+      className="border border-black px-4 py-1 hover:bg-blue-500">
+      Login
+    </button>
+  )
+} 
