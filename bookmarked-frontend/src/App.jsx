d07eced097baaf50d5e289ed1b68c4059971f2a9
tom <desa2thomas@gmail.com>
2026-03-17 20:30:50 -0400

    initalized tailwind css -downgraded vite to ^7.0.0 and the react compiler for tailwind

--- bookmarked-frontend/src/App.jsx
+++ bookmarked-frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (<> Hello world </>)}
+  return(
+    <>
+     <div  
+    className='mx-auto flwx max-w-sm item-center gap-x-4 rounded-xl bg-blue p-6 shadow-lg outline outline-black/5'>
+      Hello world 
+     </div>
+
+    </>)
+
+}
 
 export default App
+
