bd099dd6a472ca47d99c99dae95d192af0b9b3fc
tom <desa2thomas@gmail.com>
2026-03-27 12:10:50 -0400

    fixed RegitrationPopup colors

--- frontend/src/pages/RegisterPopup.jsx
+++ frontend/src/pages/RegisterPopup.jsx
@@ -15,14 +15,16 @@ export default function RegisterPopup({ closeFunction }) {
   return (
     <Overlay>
 
-      <div className={`fixed z-1 px-15 py-10 p border-black bg-bgsecondary top-1/4      flex flex-col gap-4 text-white rounded-sm justify-between h-110
-      transition-all duration-600
-      ${mounted ? "scale-100 opacity-100" : "scale-80 opacity-0"}`}>
+      <div className={`fixed z-1 px-15 py-10 p bg-bgsecondary 
+        top-1/4 flex flex-col gap-4 text-fontalt rounded-sm justify-between 
+        h-110 transition-all duration-600
+      ${mounted ? "scale-100 opacity-100" : "scale-80 opacity-0"}
+        border-fontalt border-1`}>
 
         <div className="flex items-center justify-between 
-          text-gray-100 font-light tracking-wide">
+          text-black font-light tracking-wide">
           <h1>JOIN BOOKMARKED</h1>
-          <HiXMark className="hover:fill-white w-6 h-auto cursor-pointer" onClick={closeFunction} />
+          <HiXMark className="hover:fill-red-500 w-6 h-auto cursor-pointer" onClick={closeFunction} />
         </div>
 
         <div className="flex flex-col gap-3">
@@ -31,7 +33,7 @@ export default function RegisterPopup({ closeFunction }) {
           <InputField label={"Password"} placeholder={"hplovecraft123"} />
         </div>
 
-        <button className="self-start border-gray-500 border 1 bg-green-500 px-5 py-1 rounded-sm mt-1 text-white font-semibold opacity-90 hover:opacity-100">
+        <button className="self-start border-green-700 border 1 bg-green-500 px-5 py-1 rounded-sm mt-1 text-white font-semibold opacity-90 hover:opacity-100">
           Sign Up
         </button>
       </div>
