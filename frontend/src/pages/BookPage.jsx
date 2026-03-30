4fd67b690a76a3ca9143a506c7958426c8aa8c79
tom <desa2thomas@gmail.com>
2026-03-29 21:03:20 -0400

    started RatingWidget within BookPage

--- frontend/src/pages/BookPage.jsx
+++ frontend/src/pages/BookPage.jsx
@@ -10,20 +10,22 @@ export default function BookPage({ }) {
   const book = exampleBooks.find(book => book.id == bookId)
 
   return (
-    <div className="flex p-5 justify-center">
+    <div className="flex p-5 gap-5 justify-center">
 
       {/* BOOK COVER */}
-      <div className="pr-7">
+      <div className="">
 
         <BookCoverCard src={book.img_path} size="L" />
       </div>
 
-      <div className="flex flex-col max-w-150">
+      {/* COL 2 : Title , year, author , synopsis , rating widget*/}
+      <div className="flex flex-col max-w-160">
         {/* TITLE */}
         <p
           className="font-black text-4xl font-noto-serif tracking-tight">
           {book.title}
         </p>
+        {/* YEAR - AUTHOR */}
         <div className="flex gap-3 pb-3">
           <p className="underline">
             {book.publicationYear}
@@ -32,10 +34,41 @@ export default function BookPage({ }) {
             Written by <span className="underline">{book.author}</span>
           </p>
         </div>
-        <p className="text-black font-medium tracking-tight">
-          {book.synopsis}
-        </p>
+        {/* SYNOPSIS - RATING*/}
+        <div className="flex gap-5">
+          <p className="text-black font-medium tracking-tight">
+            {book.synopsis}
+          </p>
+          <RatingWidget />
+        </div>
+      </div>
+    </div>
+  )
+}
 
+
+//WIDGET ICONS
+import { IoBookOutline } from "react-icons/io5";
+import { CiHeart } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
+function RatingWidget() {
+  const iconSize = 40
+  return (
+    <div
+      className="
+    border-1 border-black rounded-md min-w-50 h-fit overflow-hide p-4
+    flex flex-col">
+
+      {/* FIRST ROW*/}
+      <div className="flex gap-2 justify-between">
+        <IoBookOutline size={iconSize}/>
+        <CiHeart size={iconSize} />
+        <CiBookmark size={iconSize}/>
+      </div>
+
+      {/*SECOND ROW*/}
+      <div>
+        RATE (STARS)
       </div>
     </div>
   )
